0bfb06ba907893fdc5cd8f717adc6cdcc36f7653
ryanthomas92 <rthoma11@villanova.edu>
2017-02-08 10:30:57 -0800

    generated solution

--- queue.rb
+++ queue.rb
@@ -1,20 +1,35 @@
-class Queue
+require_relative 'singly_linked_list'
 
+
+class Queue
   attr_reader :front, :contents
 
-  def initialize
+  def initialize(data=nil)
+    @contents = SinglyLinkedList.new(front)
+    @front = @contents.head
   end
 
-  def enqueue
+  def enqueue(element)
+    @contents.insert_at_end(element)
   end
 
   def dequeue
+    if !@contents.empty?
+      value = @front.data
+      @contents.remove_from_start
+      @front = @contents.head
+      value
+    else
+      nil
+    end
   end
 
   def inspect
+    @contents.inspect.sub('(head)', '(front)')
   end
 
   def empty?
+    @contents.empty?
   end
 
 end
